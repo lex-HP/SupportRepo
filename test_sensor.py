8c6ba2acb7dd617290782f2845e5c01857aa6ebe
AHP-23 <alexchp@bu.edu>
2023-02-23 21:53:59 -0500

    Update test_sensor.py

--- test_sensor.py
+++ test_sensor.py
@@ -8,21 +8,20 @@ import adafruit_vl53l4cd
 print("Hello")
 print("--------------------")
 i2c = board.I2C()  # uses board.SCL and board.SDA
-vl53 = adafruit_vl53l4cd.VL53L4CD(i2c)
+ToF = adafruit_vl53l4cd.VL53L4CD(i2c)
 
 # OPTIONAL: can set non-default values
-vl53.inter_measurement = 0
-vl53.timing_budget = 200
+ToF.inter_measurement = 0
+ToF.timing_budget = 200
 
-
-
-vl53.start_ranging()
-
+ToF.start_ranging()
 for i in range(10):
-    while not vl53.data_ready:
+    while not ToF.data_ready:
         pass
-    vl53.clear_interrupt()
-    print("Distance: {} cm".format(vl53.distance))
-
+    ToF.clear_interrupt()
+    print("Distance: {} cm".format(ToF.distance))
+    if ToF.distance <= 10 and ToF.distance != 0.0:
+        flag = 1
+        print("too close")
 print("--------------------")
 print("Goodbye")

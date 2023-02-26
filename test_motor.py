4b93969e9057a579360b1d82e6aa78ea46e02a11
AHP-23 <alexchp@bu.edu>
2023-02-26 15:43:36 -0500

    Update test_motor.py

--- test_motor.py
+++ test_motor.py
@@ -9,25 +9,25 @@ print("Hello")
 ControlPin = [11, 13, 15, 16] # Set the GPIO pins for the motor
 num_steps = 512 # Set the number of step
 delay = 0.001 # Set delay between steps
-'''
+
 step_sequence = [[1,0,0,0],[1,1,0,0], # Counter-Clockwise
                  [0,1,0,0],[0,1,1,0],
                  [0,0,1,0],[0,0,1,1],
                  [0,0,0,1],[1,0,0,1]] 
 
-''' '''
+''' 
 step_sequence = [[0,1,1,1],[0,0,1,1], # Clockwise
                  [1,0,1,1],[1,0,0,1],
                  [1,1,0,1],[1,1,0,0],
                  [1,1,1,0],[0,1,1,0]] 
 
 
-'''
+''' '''
 step_sequence = [[1,0,0,1],[0,0,0,1], # Clockwise
                  [0,0,1,1],[0,0,1,0],
                  [0,1,1,0],[0,1,0,0],
                  [1,1,0,0],[1,0,0,0]]
-
+'''
 # Set up the GPIO pins
 GPIO.setmode(GPIO.BOARD)
 for pin in ControlPin:
@@ -37,8 +37,12 @@ for pin in ControlPin:
 # Run the motor
 for i in range(num_steps):
     for step in range(8):
-        for pin in range(4):
-            GPIO.output(ControlPin[pin], step_sequence[step][pin])
+        for pin in range(1):
+            GPIO.output(ControlPin[1], step_sequence[step][1])
+            GPIO.output(ControlPin[2], step_sequence[step][2])
+            GPIO.output(ControlPin[3], step_sequence[step][3])
+            GPIO.output(ControlPin[4], step_sequence[step][4])
+        
         time.sleep(delay)
 
 # Clean up the GPIO pins 

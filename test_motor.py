78f225bd1b9e628894c7f3a5f4d0f6e3a2e7c67e
AHP-23 <alexchp@bu.edu>
2023-04-27 17:45:20 -0400

    Update test_motor.py

--- test_motor.py
+++ test_motor.py
@@ -1,37 +1,36 @@
 #Simple demo of the stepper motor, does a full rotation
-
 import RPi.GPIO as GPIO
 import time
 
-print("Hello")
+def run_motor():
+    print("Hello")
 
-# Set constants
-ControlPin = [17, 27, 22, 23] # Set the GPIO pins for the motor
-num_steps = 256 # Set the number of step
-delay = 0.001 # Set delay between steps
+    # Set constants
+    ControlPin = [11, 13, 15, 16] # Set the GPIO pins for the motor
+    num_steps = 1024 # Set the number of step
+    delay = 0.0005 # Set delay between steps
+    step_sequence = [[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
+                 [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1],
+                 [1,0,0,0],[1,0,1,0],[0,0,1,0],[0,1,1,0],
+                 [0,1,0,0],[0,1,0,1],[0,0,0,1],[1,0,0,1]]
 
-step_sequence = [[1,0,0,0],[1,1,0,0], # Counter-Clockwise
-                 [0,1,0,0],[0,1,1,0],
-                 [0,0,1,0],[0,0,1,1],
-                 [0,0,0,1],[1,0,0,1]] 
 
-# Set up the GPIO pins
-GPIO.setmode(GPIO.BCM)
-for pin in ControlPin:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin,0)
+    # Set up the GPIO pins
+    GPIO.setmode(GPIO.BOARD)
+    for pin in ControlPin:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin,0)
 
-# Run the motor
-for i in range(num_steps):
-    for step in range(8):
-        for pin in range(1):
-            GPIO.output(ControlPin[0], step_sequence[step][0])
-            GPIO.output(ControlPin[2], step_sequence[step][2])
-            GPIO.output(ControlPin[1], step_sequence[step][1])
-            GPIO.output(ControlPin[3], step_sequence[step][3])
-        
-        time.sleep(delay)
+    # Run the motor
+    for i in range(num_steps):
+        for step in range(8):
+            for pin in range(4):
+                GPIO.output(ControlPin[pin], step_sequence[step][pin])
+            time.sleep(delay)
 
-# Clean up the GPIO pins 
-GPIO.cleanup()
-print("Goodbye")
+    # Clean up the GPIO pins 
+    GPIO.cleanup()
+    print("Goodbye")
+
+if __name__ == "__main__":
+    run_motor()

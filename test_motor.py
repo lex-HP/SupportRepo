bd1d9b91889a77df42d85a57b4b1cff4cf9978ef
AHP-23 <alexchp@bu.edu>
2023-04-27 19:10:30 -0400

    Update test_motor.py

--- test_motor.py
+++ test_motor.py
@@ -10,10 +10,12 @@ ControlPin = [18, 27, 22, 23] # Set the GPIO pins for the motor
 num_steps = 512 # Set the number of step
 delay = 0.001 # Set delay between steps
 
-step_sequence = [[1,0,0,0],[1,1,0,0], # Counter-Clockwise
+step_sequence = [[1,0,0,0],[1,1,0,0], 
                  [0,1,0,0],[0,1,1,0],
                  [0,0,1,0],[0,0,1,1],
                  [0,0,0,1],[1,0,0,1]] 
+inverted_sequence = [[1^bit for bit in step] for step in step_sequence]
+step_sequence = inverted_sequence
 
 # Set up the GPIO pins
 GPIO.setmode(GPIO.BCM)

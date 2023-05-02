552f2e955216da9a4285132632164a7677a80e2c
AHP-23 <alexchp@bu.edu>
2023-05-02 13:03:33 -0400

    Update test_pin.py

--- test_pin.py
+++ test_pin.py
@@ -4,10 +4,10 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO_pin = int(input("Give physical pin number: "))
 
-for pin in GPIO_pin:
-    GPIO.setup(pin, GPIO.OUT)
+GPIO.setup(pin, GPIO.OUT)
+start_time = time.time()
 
-while True:
+while (time.time() - start_time) < 5:
     GPIO.output(pin, GPIO.HIGH)
     time.sleep(0.5)
     GPIO.output(pin, GPIO.LOW)

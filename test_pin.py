57838d988c83f430447fbc8fbaee4bb184a65da6
AHP-23 <alexchp@bu.edu>
2023-05-02 13:00:57 -0400

    Create test_pin.py

---
+++ test_pin.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO_pin = input("Give physical pin number: ")
+
+for pin in GPIO_pin:
+    GPIO.setup(pin, GPIO.OUT)
+
+while True:
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(pin, GPIO.LOW)
+
+# clean up GPIO
+GPIO.cleanup()
